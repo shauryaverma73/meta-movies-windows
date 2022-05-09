c683015ed36fee0071df77269a071ecc309b6d5b
shauryaverma_73 <maskedanonxx@gmail.com>
2022-05-10 00:30:39 +0530

    last commit message + catalogue started

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 	/*==============================
 	Menu
 	==============================*/
-	$('.header__btn').on('click', function() {
+	$('.header__btn').on('click', function () {
 		$(this).toggleClass('header__btn--active');
 		$('.header__nav').toggleClass('header__nav--active');
 		$('.body').toggleClass('body--active');
@@ -18,7 +18,7 @@ $(document).ready(function () {
 	/*==============================
 	Search
 	==============================*/
-	$('.header__search-btn').on('click', function() {
+	$('.header__search-btn').on('click', function () {
 		$(this).toggleClass('active');
 		$('.header__search').toggleClass('header__search--active');
 
@@ -45,8 +45,8 @@ $(document).ready(function () {
 		margin: 0,
 	});
 
-	$('.home__bg .item').each( function() {
-		if ($(this).attr("data-bg")){
+	$('.home__bg .item').each(function () {
+		if ($(this).attr("data-bg")) {
 			$(this).css({
 				'background': 'url(' + $(this).data('bg') + ')',
 				'background-position': 'center center',
@@ -64,33 +64,33 @@ $(document).ready(function () {
 		autoplay: false,
 		smartSpeed: 600,
 		margin: 30,
-		responsive : {
-			0 : {
+		responsive: {
+			0: {
 				items: 2,
 			},
-			576 : {
+			576: {
 				items: 2,
 			},
-			768 : {
+			768: {
 				items: 3,
 			},
-			992 : {
+			992: {
 				items: 4,
 			},
-			1200 : {
+			1200: {
 				items: 4,
 			},
 		}
 	});
 
-	$('.home__nav--next').on('click', function() {
+	$('.home__nav--next').on('click', function () {
 		$('.home__carousel, .home__bg').trigger('next.owl.carousel');
 	});
-	$('.home__nav--prev').on('click', function() {
+	$('.home__nav--prev').on('click', function () {
 		$('.home__carousel, .home__bg').trigger('prev.owl.carousel');
 	});
 
-	$(window).on('resize', function() {
+	$(window).on('resize', function () {
 		var itemHeight = $('.home__bg').height();
 		$('.home__bg .item').css("height", itemHeight + "px");
 	});
@@ -99,22 +99,22 @@ $(document).ready(function () {
 	/*==============================
 	Tabs
 	==============================*/
-	$('.content__mobile-tabs-menu li').each( function() {
+	$('.content__mobile-tabs-menu li').each(function () {
 		$(this).attr('data-value', $(this).text().toLowerCase());
 	});
 
-	$('.content__mobile-tabs-menu li').on('click', function() {
+	$('.content__mobile-tabs-menu li').on('click', function () {
 		var text = $(this).text();
 		var item = $(this);
 		var id = item.closest('.content__mobile-tabs').attr('id');
-		$('#'+id).find('.content__mobile-tabs-btn input').val(text);
+		$('#' + id).find('.content__mobile-tabs-btn input').val(text);
 	});
 
 	/*==============================
 	Section bg
 	==============================*/
-	$('.section--bg, .details__bg').each( function() {
-		if ($(this).attr("data-bg")){
+	$('.section--bg, .details__bg').each(function () {
+		if ($(this).attr("data-bg")) {
 			$(this).css({
 				'background': 'url(' + $(this).data('bg') + ')',
 				'background-position': 'center center',
@@ -127,15 +127,15 @@ $(document).ready(function () {
 	/*==============================
 	Filter
 	==============================*/
-	$('.filter__item-menu li').each( function() {
+	$('.filter__item-menu li').each(function () {
 		$(this).attr('data-value', $(this).text().toLowerCase());
 	});
 
-	$('.filter__item-menu li').on('click', function() {
+	$('.filter__item-menu li').on('click', function () {
 		var text = $(this).text();
 		var item = $(this);
 		var id = item.closest('.filter__item').attr('id');
-		$('#'+id).find('.filter__item-btn input').val(text);
+		$('#' + id).find('.filter__item-btn input').val(text);
 	});
 
 	/*==============================
@@ -169,10 +169,10 @@ $(document).ready(function () {
 	/*==============================
 	Gallery
 	==============================*/
-	var initPhotoSwipeFromDOM = function(gallerySelector) {
+	var initPhotoSwipeFromDOM = function (gallerySelector) {
 		// parse slide data (url, title, size ...) from DOM elements 
 		// (children of gallerySelector)
-		var parseThumbnailElements = function(el) {
+		var parseThumbnailElements = function (el) {
 			var thumbElements = el.childNodes,
 				numNodes = thumbElements.length,
 				items = [],
@@ -181,12 +181,12 @@ $(document).ready(function () {
 				size,
 				item;
 
-			for(var i = 0; i < numNodes; i++) {
+			for (var i = 0; i < numNodes; i++) {
 
 				figureEl = thumbElements[i]; // <figure> element
 
 				// include only element nodes 
-				if(figureEl.nodeType !== 1) {
+				if (figureEl.nodeType !== 1) {
 					continue;
 				}
 
@@ -201,15 +201,15 @@ $(document).ready(function () {
 					h: parseInt(size[1], 10)
 				};
 
-				if(figureEl.children.length > 1) {
+				if (figureEl.children.length > 1) {
 					// <figcaption> content
-					item.title = figureEl.children[1].innerHTML; 
+					item.title = figureEl.children[1].innerHTML;
 				}
 
-				if(linkEl.children.length > 0) {
+				if (linkEl.children.length > 0) {
 					// <img> thumbnail element, retrieving thumbnail url
 					item.msrc = linkEl.children[0].getAttribute('src');
-				} 
+				}
 
 				item.el = figureEl; // save link to element for getThumbBoundsFn
 				items.push(item);
@@ -220,22 +220,22 @@ $(document).ready(function () {
 
 		// find nearest parent element
 		var closest = function closest(el, fn) {
-			return el && ( fn(el) ? el : closest(el.parentNode, fn) );
+			return el && (fn(el) ? el : closest(el.parentNode, fn));
 		};
 
 		// triggers when user clicks on thumbnail
-		var onThumbnailsClick = function(e) {
+		var onThumbnailsClick = function (e) {
 			e = e || window.event;
 			e.preventDefault ? e.preventDefault() : e.returnValue = false;
 
 			var eTarget = e.target || e.srcElement;
 
 			// find root element of slide
-			var clickedListItem = closest(eTarget, function(el) {
+			var clickedListItem = closest(eTarget, function (el) {
 				return (el.tagName && el.tagName.toUpperCase() === 'FIGURE');
 			});
 
-			if(!clickedListItem) {
+			if (!clickedListItem) {
 				return;
 			}
 
@@ -248,53 +248,53 @@ $(document).ready(function () {
 				index;
 
 			for (var i = 0; i < numChildNodes; i++) {
-				if(childNodes[i].nodeType !== 1) { 
-					continue; 
+				if (childNodes[i].nodeType !== 1) {
+					continue;
 				}
 
-				if(childNodes[i] === clickedListItem) {
+				if (childNodes[i] === clickedListItem) {
 					index = nodeIndex;
 					break;
 				}
 				nodeIndex++;
 			}
 
-			if(index >= 0) {
+			if (index >= 0) {
 				// open PhotoSwipe if valid index found
-				openPhotoSwipe( index, clickedGallery );
+				openPhotoSwipe(index, clickedGallery);
 			}
 			return false;
 		};
 
 		// parse picture index and gallery index from URL (#&pid=1&gid=2)
-		var photoswipeParseHash = function() {
+		var photoswipeParseHash = function () {
 			var hash = window.location.hash.substring(1),
-			params = {};
+				params = {};
 
-			if(hash.length < 5) {
+			if (hash.length < 5) {
 				return params;
 			}
 
 			var vars = hash.split('&');
 			for (var i = 0; i < vars.length; i++) {
-				if(!vars[i]) {
+				if (!vars[i]) {
 					continue;
 				}
-				var pair = vars[i].split('=');  
-				if(pair.length < 2) {
+				var pair = vars[i].split('=');
+				if (pair.length < 2) {
 					continue;
-				}           
+				}
 				params[pair[0]] = pair[1];
 			}
 
-			if(params.gid) {
+			if (params.gid) {
 				params.gid = parseInt(params.gid, 10);
 			}
 
 			return params;
 		};
 
-		var openPhotoSwipe = function(index, galleryElement, disableAnimation, fromURL) {
+		var openPhotoSwipe = function (index, galleryElement, disableAnimation, fromURL) {
 			var pswpElement = document.querySelectorAll('.pswp')[0],
 				gallery,
 				options,
@@ -308,24 +308,24 @@ $(document).ready(function () {
 				// define gallery index (for URL)
 				galleryUID: galleryElement.getAttribute('data-pswp-uid'),
 
-				getThumbBoundsFn: function(index) {
+				getThumbBoundsFn: function (index) {
 					// See Options -> getThumbBoundsFn section of documentation for more info
 					var thumbnail = items[index].el.getElementsByTagName('img')[0], // find thumbnail
 						pageYScroll = window.pageYOffset || document.documentElement.scrollTop,
-						rect = thumbnail.getBoundingClientRect(); 
+						rect = thumbnail.getBoundingClientRect();
 
-					return {x:rect.left, y:rect.top + pageYScroll, w:rect.width};
+					return { x: rect.left, y: rect.top + pageYScroll, w: rect.width };
 				}
 
 			};
 
 			// PhotoSwipe opened from URL
-			if(fromURL) {
-				if(options.galleryPIDs) {
+			if (fromURL) {
+				if (options.galleryPIDs) {
 					// parse real index when custom PIDs are used 
 					// http://photoswipe.com/documentation/faq.html#custom-pid-in-url
-					for(var j = 0; j < items.length; j++) {
-						if(items[j].pid == index) {
+					for (var j = 0; j < items.length; j++) {
+						if (items[j].pid == index) {
 							options.index = j;
 							break;
 						}
@@ -339,31 +339,31 @@ $(document).ready(function () {
 			}
 
 			// exit if index not found
-			if( isNaN(options.index) ) {
+			if (isNaN(options.index)) {
 				return;
 			}
 
-			if(disableAnimation) {
+			if (disableAnimation) {
 				options.showAnimationDuration = 0;
 			}
 
 			// Pass data to PhotoSwipe and initialize it
-			gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+			gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
 			gallery.init();
 		};
 
 		// loop through all gallery elements and bind events
-		var galleryElements = document.querySelectorAll( gallerySelector );
+		var galleryElements = document.querySelectorAll(gallerySelector);
 
-		for(var i = 0, l = galleryElements.length; i < l; i++) {
-			galleryElements[i].setAttribute('data-pswp-uid', i+1);
+		for (var i = 0, l = galleryElements.length; i < l; i++) {
+			galleryElements[i].setAttribute('data-pswp-uid', i + 1);
 			galleryElements[i].onclick = onThumbnailsClick;
 		}
 
 		// Parse URL and open gallery if it contains #&pid=3&gid=1
 		var hashData = photoswipeParseHash();
-		if(hashData.pid && hashData.gid) {
-			openPhotoSwipe( hashData.pid ,  galleryElements[ hashData.gid - 1 ], true, true );
+		if (hashData.pid && hashData.gid) {
+			openPhotoSwipe(hashData.pid, galleryElements[hashData.gid - 1], true, true);
 		}
 	};
 	// execute above function
@@ -392,11 +392,11 @@ $(document).ready(function () {
 			noUiSlider.create(firstSlider, {
 				range: {
 					'min': 2000,
-					'max': 2018
+					'max': 2022
 				},
 				step: 1,
 				connect: true,
-				start: [2005, 2015],
+				start: [2005, 2022],
 				format: wNumb({
 					decimals: 0,
 				})
@@ -405,7 +405,7 @@ $(document).ready(function () {
 				document.getElementById('filter__years-start'),
 				document.getElementById('filter__years-end')
 			];
-			firstSlider.noUiSlider.on('update', function( values, handle ) {
+			firstSlider.noUiSlider.on('update', function (values, handle) {
 				firstValues[handle].innerHTML = values[handle];
 			});
 		} else {
@@ -437,7 +437,7 @@ $(document).ready(function () {
 				document.getElementById('filter__imbd-end')
 			];
 
-			secondSlider.noUiSlider.on('update', function( values, handle ) {
+			secondSlider.noUiSlider.on('update', function (values, handle) {
 				secondValues[handle].innerHTML = values[handle];
 			});
 
@@ -471,7 +471,7 @@ $(document).ready(function () {
 
 			var thirdValue = document.getElementById('form__slider-value');
 
-			thirdSlider.noUiSlider.on('update', function( values, handle ) {
+			thirdSlider.noUiSlider.on('update', function (values, handle) {
 				thirdValue.innerHTML = values[handle];
 			});
 		} else {
@@ -481,3 +481,16 @@ $(document).ready(function () {
 	}
 	$(window).on('load', initializeThirdSlider());
 });
+
+// ***************************
+document.getElementById('apply').addEventListener('click', () => {
+	const genre = document.getElementById('genreName').value;
+	const ratingLow = document.getElementById('filter__imbd-start').innerHTML;
+	const ratingHigh = document.getElementById('filter__imbd-end').innerHTML;
+	const year = document.getElementById('filter__years-end').innerHTML;
+	//- create a link with data to make api request api
+	alert(genre + ' ' + ratingLow + ' ' + ratingHigh + ' ' + year);
+	const apiLink = `/api/v1/movie?ratings[gte]=${ratingLow}&ratings[lte]=${ratingHigh}&year[eq]=${year}&genre=${genre}`;
+	alert(apiLink);
+});
+// ***************************
