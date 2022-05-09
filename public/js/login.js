263eeae6e7dece9fba5d724e464ba87dacbb22c5
shauryaverma_73 <maskedanonxx@gmail.com>
2022-05-09 22:06:26 +0530

    dollar to rupees changed in subscription page

---
+++ public/js/login.js
@@ -0,0 +1,13 @@
+
+const login = (email, password) => {
+    alert('login clicked');
+};
+
+document.querySelector('.sign__form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.getElementById('.email');
+    const password = document.getElementById('.password ');
+
+    login(email, password);
+
+});
